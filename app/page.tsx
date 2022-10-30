752f0419c52d2b0e234b94ce05c5d61e91cf541f
jonandrewdavis <jonandrewdavis@gmail.com>
2022-10-30 13:01:54 -0400

    initial project finally fixed, with working tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
 import { use } from 'react';
-import { fetchTransactions } from '@/lib/getCategories';
+import { fetchTransactions } from '/lib/getCategories';
 
 const tablehead = [
-  "Transaction Date", "Post Date",
+  "Post Date",
   "Description", "Category", "Type", "Amount"
 ]
 
@@ -19,25 +22,37 @@ export default function Page() {
   const transactionsData = use(fetchLocal());
   if (!transactionsData) return null;
   return (
-    <div className="space-y-6 dark">
-      <header>
-        <h1 className="w-screen text-zinc-500 text-7xl" >2: UPDATE Monthly</h1>
-      </header>
-      <div className="w-screen p12  flex-1">
-        <table className="table-auto text-sm text-left text-zinc-400">
-          <thead className="text-xs text-gray-700 uppercase">
-            <tr>
-              {tablehead.map(head => <th className="text-zinc-500 text-s">{head}</th>)}
-            </tr>
-          </thead>
-          <tbody>
-            {transactionsData.map((transaction: Transaction) => (
-              <tr className="border-gray">
-                {tablehead.map((column: string) => <td className="py-4 px-6 text-white">{transaction[column]}</td>)}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <div>
+      <div className="max-w-4xl mx-auto">
+        <header className='p-8'>
+          <h1 className="text-vercel-pink text-3xl" >monthly 2</h1>
+        </header>
+        <div className="flex flex-col">
+          <div className="overflow-x-auto shadow-md sm:rounded-lg">
+            <div className="inline-block min-w-full align-middle">
+              <div className="overflow-hidden ">
+                <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                  <thead className="bg-gray-100 dark:bg-gray-700">
+                    <tr>
+                      {tablehead.map(head => (
+                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                          {head}
+                        </th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                    {transactionsData.map((transaction: Transaction) => (
+                      <tr scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                        {tablehead.map((column: string) => <td className="py-4 px-6 text-white">{transaction[column]}</td>)}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
